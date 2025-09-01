63e1f6a487cadc23fb06961b75509bb3d7c901da
Alish <alishbaig2004@gmail.com>
2025-09-01 15:09:36 +0530

    refactor: move landing page to its own route group

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,14 @@
+import AuthButton from '@/components/auth-button'
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div>
+        <AuthButton />
+
+    </div>
+  )
+}
+
+export default LandingPage
